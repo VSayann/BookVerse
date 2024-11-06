4e2b229baaf6361e1ce3137fc3d2aee931503bd6
VSayann <sayann.valmond@gmail.com>
2024-11-06 10:22:25 +0100

    Add static file serving and homepage route in app.js

--- bookverse/backend/app.js
+++ bookverse/backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const clubRoutes = require('./routes/clubRoutes');
 
 dotenv.config();
@@ -11,6 +12,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 
 app.use('/api/clubs', clubRoutes);
 
