e0e5979ffc2e6c669a0efdb752e8ad83d190f13c
VSayann <sayann.valmond@gmail.com>
2024-11-06 11:16:32 +0100

    Add User model with Sequelize for database integration

---
+++ bookverse/backend/models/index.js
@@ -0,0 +1,19 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+});
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = { sequelize, User };
